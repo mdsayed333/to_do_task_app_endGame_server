9be68cce64040a60deaf93d7c4fd2b867be314d3
Sayed <mdsayedgv@gmail.com>
2022-07-02 03:27:22 +0600

    some correction

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
-/**
- toDo_app_endgame
- 2CmIT9vccVEdaIF6
- * 
- */
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i3kvt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -21,15 +16,15 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-
 async function run() {
   try {
     await client.connect();
     const taskCollection = client.db("todo-task").collection("task");
-    const completeTaskCollection = client.db("todo-task").collection("complete-task");
+    const completeTaskCollection = client
+      .db("todo-task")
+      .collection("complete-task");
 
     console.log("MongoDB is running.......");
-
 
     app.get("/task", async (req, res) => {
       const tasks = await taskCollection.find().toArray();
@@ -49,9 +44,6 @@ async function run() {
       res.send(result);
     });
 
-
-
-
     // Call from update task
     app.put("/task/:id", async (req, res) => {
       const id = req.params.id;
@@ -63,16 +55,9 @@ async function run() {
           task: newTask.task,
         },
       };
-      const result = await taskCollection.updateOne(
-        filter,
-        updateTask,
-        option
-      );
+      const result = await taskCollection.updateOne(filter, updateTask, option);
       res.send(result);
     });
-
-
-
 
     app.get("/complete", async (req, res) => {
       const tasks = await completeTaskCollection.find().toArray();
@@ -84,27 +69,14 @@ async function run() {
       const result = await completeTaskCollection.insertOne(task);
       res.send(result);
     });
-
-
-
   } finally {
   }
 }
 
 run().catch(console.dir);
 
-
-
-
-
-
-
-
-
-
-
 app.get("/", (req, res) => {
-  res.send("Hello from To Do Task app For Endgame!!!");
+  res.send("Hello To Do Task app For Endgame!!!");
 });
 
 app.listen(port, () => {
